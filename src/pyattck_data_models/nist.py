3bfe18b3ada03a8b97b462e5fe7fe80121485b6d
MSAdministrator <rickardja@live.com>
2022-05-26 14:20:51 -0500

    fix: Added support for GeneratedNistControlMap model

--- src/pyattck_data_models/nist.py
+++ src/pyattck_data_models/nist.py
@@ -40,3 +40,25 @@ class NistControls:
                 except Exception as e:
                     raise e
             self.objects = return_list
+
+@define
+class GeneratedNistControlMap:
+    data: dict = field()
+
+    def __attrs_post_init__(self):
+        if self.data:
+            return_dict = {}
+            for key,val in self.data.items():
+                try:
+                    Id().validate(key)
+                except Exception as e:
+                    raise e
+                return_dict[key] = []
+                if isinstance(val, list):
+                    for item in val:
+                        try:
+                            Id().validate(item)
+                        except Exception as e:
+                            raise e
+                    return_dict[key] = val
+            self.data = return_dict
