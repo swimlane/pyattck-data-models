b752d5873a2fa7a742aeba459e93a60aad45d472
MSAdministrator <rickardja@live.com>
2022-06-09 11:16:27 -0500

    dev: Update actor model

--- src/pyattck_data_models/actor.py
+++ src/pyattck_data_models/actor.py
@@ -31,7 +31,7 @@ class Actor(BaseModel):
 
     # These additional properties are from external data sets
     names:                       List              = field(factory=list)
-    tools:                       List              = field(factory=list)
+    #tools:                       List              = field(factory=list)
     external_tools:              List              = field(factory=list)
     country:                     List              = field(factory=list)
     operations:                  List              = field(factory=list)
